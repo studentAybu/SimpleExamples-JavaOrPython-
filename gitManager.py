63fbb73e4460790712c0ea01e9aa9c4fd83d27e1
studentAybu <23050161004@aybu.edu.tr>
2025-01-29 21:56:05 +0300

    "comment"

--- gitManager.py
+++ gitManager.py
@@ -1,6 +1,16 @@
 import os
+import subprocess
 
-def git_Add():
+def git_Add1_system():
     os.system('git add gitManager.py && git commit -m "comment" && git push origin main')
 
-git_Add()
+
+
+def git_Add2_subprocess():
+    subprocess.run(['git', 'add', 'gitManager.py'])
+    subprocess.run(['git', 'commit', '-m', '"comment"'])
+    subprocess.run(['git', 'remote', 'add', 'origin', 'git push origin main'])
+
+#git_Add1_system()
+git_Add2_subprocess()
+
