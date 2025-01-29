f70241b0e9ab502e1ceafdaf7c31a50570175f44
studentAybu <23050161004@aybu.edu.tr>
2025-01-29 13:26:28 +0300

    Add gitManager.py

--- gitManager.py
+++ gitManager.py
@@ -1,4 +1,4 @@
 import os
 
-# Dosyayı ekleyip, commit ve push işlemini birleştirelim
-os.system('cd C:\\Users\\Macbook\\Documents\\pythonProjects\\SimpleExamples-JavaOrPython- && git add gitManager.py && git commit -m "Add gitManager.py" && git remote add origin https://github.com/studentAybu/SimpleExamples-JavaOrPython-.git && git push -u origin main')
+# Dosyayı ekle, commit yap ve push işlemini gerçekleştir
+os.system('git add gitManager.py && git commit -m "Add gitManager.py" && git push origin main')
