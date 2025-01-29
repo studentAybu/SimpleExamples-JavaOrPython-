76e6f6175741be467295f1d60d65a51c162c1c07
studentAybu <23050161004@aybu.edu.tr>
2025-01-29 13:31:43 +0300

    comment

--- gitManager.py
+++ gitManager.py
@@ -1,6 +1,6 @@
 import os
 
 def git_Add():
-    os.system('git add gitManager.py && git commit -m "Add gitManager.py" && git push origin main')
+    os.system('git add gitManager.py && git commit -m "comment" && git push origin main')
 
 git_Add()
